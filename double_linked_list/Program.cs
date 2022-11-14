6c955287a0b2fb0794a1c247c53829ba5461aac5
SalmanLS <salmanluthfi456@gmail.com>
2022-11-14 17:34:29 +0700

    create 2 if clause

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -76,7 +76,27 @@ namespace double_linked_list
             previous = current = null;
             if (search(rollNo, ref previous, ref current) == false)
                 return false;
-
+            // the begining of the data
+            if(current.next == null)
+            {
+                previous.next = null;
+                return true;
+            }
+            //node between 2 node in the list
+            if (current == START)
+            {
+                START = START.next;
+                if(START != null)
+                {
+                    START.prev = null;
+                    return true;
+                }
+            }
+            /*if the to be deleted is in between the list then the following lines of is executed*/
+            previous.next = current.next;
+            current.next.prev = previous;
+            return true;
         }
+
     }
 }
