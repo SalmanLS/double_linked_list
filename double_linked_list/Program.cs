39a9c9b48f6aec2850c831a3b184a7dbe4270155
SalmanLS <salmanluthfi456@gmail.com>
2022-11-14 20:00:24 +0700

    create menu and switch case 1

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -137,7 +137,31 @@ namespace double_linked_list
     {
         static void Main(string[] args)
         {
-
+            DoubleLinkedList obj = new DoubleLinkedList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\n Menu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all records in the ascending order of roll numbers");
+                    Console.WriteLine("4. View all records in the descending order of roll numbers");
+                    Console.WriteLine("5. Search for a record in the list");
+                    Console.WriteLine("6. Exit\n");
+                    Console.Write("Enter your choice (1-6): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addnode();
+                            }
+                            break;
+                    }
+                        
+                }
+            }
         }
     }
 }
